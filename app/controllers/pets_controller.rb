1948cb7ef8f09c65e82a6fc96522aa72ec29942b
“Edward <eminaya49@gmail.com>
2023-06-20 10:48:19 -0500

    created update action and tested in http request

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -26,9 +26,30 @@ class PetsController < ApplicationController
       cat_social: params[:cat_social],
       kid_compatibility: params[:kid_compatibility],
       full_description: params[:full_description],
-
     )
     @pet.save
+    render :show
+  end
+
+  def update
+    @pet = Pet.find_by(id: params[:id])
+    @pet.update(
+      name: params[:name] || @pet.name,
+      breed: params[:breed] || @pet.breed,
+      sex: params[:sex] || @pet.sex,
+      weight: params[:weight] || @pet.weight,
+      height: params[:height] || @pet.height,
+      length: params[:length] || @pet.length,
+      age: params[:age] || @pet.age,
+      location: params[:location] || @pet.location,
+      introduction: params[:introduction] || @pet.introduction,
+      housebroken: params[:housebroken] || @pet.housebroken,
+      crate_trained: params[:crate_trained] || @pet.crate_trained,
+      dog_social: params[:dog_social] || @pet.dog_social,
+      cat_social: params[:cat_social] || @pet.cat_social,
+      kid_compatibility: params[:kid_compatibility] || @pet.kid_compatibility,
+      full_description: params[:full_description] || @pet.full_description,
+    )
     render :show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "/pets" => "pets#index"
   get "/pets/:id" => "pets#show"
   post "/pets" => "pets#create"
+  patch "/pets/:id" => "pets#update"
 end
